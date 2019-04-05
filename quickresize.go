f406c1381d01f3618a6ed8a008504e99f81ae98a
lazyjul <45462701+lazyjul@users.noreply.github.com>
2019-04-05 21:52:44 +0200

    paste
    resizing functionality

--- quickresize.go
+++ quickresize.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"image/jpeg"
+	"log"
+	"os"
 
+	"github.com/nfnt/resize"
+)
+
+func main() {
+	// open "test.jpg"
+	file, err := os.Open("test.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// decode jpeg into image.Image
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
+
+	// resize to width 1000 using Lanczos resampling
+	// and preserve aspect ratio
+	m := resize.Resize(1000, 0, img, resize.Lanczos3)
+
+	out, err := os.Create("test_resized.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	// write new image to file
+	jpeg.Encode(out, m, nil)
 }
