65c3c257bef8661600e021cbcf9af93f433941c9
lazyjul <45462701+lazyjul@users.noreply.github.com>
2019-04-05 22:10:53 +0200

    Update quickresize.go
    png > jpg
    set external arg

--- quickresize.go
+++ quickresize.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"image/jpeg"
+	"image/png"
 	"log"
 	"os"
 
@@ -10,13 +10,13 @@ import (
 
 func main() {
 	// open "test.jpg"
-	file, err := os.Open("test.jpg")
+	file, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// decode jpeg into image.Image
-	img, err := jpeg.Decode(file)
+	// decode jpg into image.Image
+	img, err := png.Decode(file)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,14 +24,14 @@ func main() {
 
 	// resize to width 1000 using Lanczos resampling
 	// and preserve aspect ratio
-	m := resize.Resize(1000, 0, img, resize.Lanczos3)
+	m := resize.Resize(100, 0, img, resize.Lanczos3)
 
-	out, err := os.Create("test_resized.jpg")
+	out, err := os.Create("test_resized.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer out.Close()
 
 	// write new image to file
-	jpeg.Encode(out, m, nil)
+	png.Encode(out, m)
 }
